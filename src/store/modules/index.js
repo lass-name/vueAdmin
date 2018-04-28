5b9260ba6480e494f58a7896a865acd1273b3ebc
lass_name <zhangdianlian@163.com>
2018-04-28 10:32:04 +0800

    修改store数据的配置方式

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,5 +1,19 @@
-import gank from './gank'
+// import gank from './gank'
 
-const modules = {gank}
+// const modules = {gank}
+// console.log(modules)
+
+// export default modules
+
+let modules = {}
+const requireModule = require.context('.', false, /\.js$/)
+requireModule.keys().forEach(fileName => {
+  if (fileName === './index.js') return
+  let moduleName = fileName.replace(/\.\/|\.js/g, '')
+  // console.log(moduleName)
+  modules[moduleName] = { ...requireModule(fileName).default }
+})
+// console.log(modules)
+// console.dir(modules)
 
 export default modules
